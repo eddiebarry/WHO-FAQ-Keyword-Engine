028840069250d9375fea243abd5809af3deec300
eddiebary <edgarmonis@gmail.com>
2020-08-21 05:57:26 +0000

    adding keyword search functionality

---
+++ keyword_extractor.py
@@ -0,0 +1,30 @@
+import json
+from collections import defaultdict
+
+class KeywordExtract:
+    def __init__(self, config):
+        self.config = config
+        self.dict = self.parse_config(self.config)
+    
+    def parse_regex_query(self, query):
+        boosting_tokens = defaultdict(str)
+        for fields in self.dict:
+            list_words = self.config[fields]
+            for wrd in list_words:
+                if wrd in query:
+                    boosting_tokens[fields] = \
+                    (boosting_tokens[fields] + " " + wrd).strip()
+        
+        return dict(boosting_tokens)
+
+    def parse_config(self, config):
+        return config
+
+if __name__ == '__main__':
+    jsonpath = "./test_excel_data/curated_keywords.json"
+    f = open(jsonpath,)
+    jsonObj = json.load(f)
+    
+    ext = KeywordExtract(jsonObj)
+    query = "what is the life expectancy of my child ?"
+    print(ext.parse_regex_query(query))
--- test_excel_data/process_data_into_json.py
+++ test_excel_data/process_data_into_json.py
@@ -1,13 +1,39 @@
 import pandas as pd
 import json
 import hashlib
+import re
+
 
 xl = pd.read_excel("./VSN Categorization Template V3.xlsx")
 xl = xl.fillna("")
 print(xl.head())
 
+config_dict = {}
 for x in xl.columns:
-    print(x)
+    if x in ["Master Question", "Master Answer"]:
+        continue
+    config_dict[x]= list(xl[x].unique())
+
+def process_config_dict(dic):
+    new_dict = {}
+    for x in dic:
+        new_list = []
+        for string in dic[x]:
+            string = string.replace('(',',').replace(')',',').replace('/',',')
+            new_ = string.lower().split(',')
+            new = [x.strip() for x in new_]
+            new_list.extend(new)
+        
+        if "" in new_list:
+            new_list.remove("")
+
+        new_dict[x]= list(set(new_list))
+    
+    return new_dict
+
+        
+
+config_dict = process_config_dict(config_dict)
 
 for idx,x in xl.iterrows():
     object_dict = dict(x)
@@ -16,4 +42,7 @@ for idx,x in xl.iterrows():
     with open(json_file_name , 'w') as json_file:
         json.dump(object_dict, json_file, indent = 4, sort_keys=True)
 
-# list of unique labels
+json_file_name = "unique_keywords.json"
+with open(json_file_name , 'w') as json_file:
+    json.dump(config_dict, json_file, indent = 4, sort_keys=True)
+
